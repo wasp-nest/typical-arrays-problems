62e8c8c4c914f35e081e40819eb31243c373b738
wasp-nest <elena_zimakova@epam.com>
2020-10-03 19:31:49 +0300

    init: start typical arrays problems-task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+
+  let index = array[0];
+
+  for (let i = 1; i < array.length; i++){
+     if (array[i] < index) {
+       index = array[i];
+     } 
+  }
+  return index;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+  
+  let index = array[0];
+
+  for (let i = 1; i < array.length; i++){
+     if (array[i] > index) {
+       index = array[i];
+     } 
+  }
+  return index;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+  let index = 0;
+  for (let i = 0; i < array.length; i++ ) {
+     index += array[i];
+  }
+  return index/array.length;
 }
